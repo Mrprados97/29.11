2ea1401d826e27af4de0636d3c00ae8a45e4ff95
Mrprados97 <mrprados97@gmail.com>
2023-03-03 19:43:33 +0200

    add cssbaseline

--- src/container/App/App.tsx
+++ src/container/App/App.tsx
@@ -1,11 +1,13 @@
 import Footer from '../Footer/Footer'
 import Header from '../Header/Header'
 import Main from '../Main/Main'
+import CssBaseline from '@mui/material/CssBaseline'
 
 type Props = {}
 const App = (props: Props) => {
     return (
         <div>
+            <CssBaseline />
             <Header />
             <Main />
             <Footer />
