3e47b508fc6353039b3ca258ae6302eef04eb974
Mrprados97 <mrprados97@gmail.com>
2023-03-03 19:29:23 +0200

    new

--- src/index.tsx
+++ src/index.tsx
@@ -1,32 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-// const title = React.createElement(
-//     'h1',
-//     {
-//         id: 'title',
-//         className: 'red',
-//     },
-
-//     'Hello React'
-// )
-
-const title = <h1 className="red">Test</h1>
-
-const list = (
-    <ul>
-        <li>List Item 1</li>
-        <li>List Item 2</li>
-        <li>List Item 3</li>
-    </ul>
-)
-
-const content = (
-    <div>
-        {title}
-        {list}
-    </div>
-)
+import App from './container/App/App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-root.render(<React.StrictMode>{content}</React.StrictMode>)
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+)
