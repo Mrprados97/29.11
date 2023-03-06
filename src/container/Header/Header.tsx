98a02b91dfb6ccd930747d00e6a6d9f572da502c
Mrprados97 <mrprados97@gmail.com>
2023-03-06 18:59:05 +0200

    add menu

--- src/container/Header/Header.tsx
+++ src/container/Header/Header.tsx
@@ -1,18 +1,17 @@
-import * as React from 'react'
 import AppBar from '@mui/material/AppBar'
-import Box from '@mui/material/Box'
+
 import Toolbar from '@mui/material/Toolbar'
-import Typography from '@mui/material/Typography'
-import Button from '@mui/material/Button'
+
 import IconButton from '@mui/material/IconButton'
 import MenuIcon from '@mui/icons-material/Menu'
 import { Container } from '@mui/material'
 import './Header.css'
 import Logo from 'components/Logo/Logo'
+import Menu from 'components/Menu/Menu'
+import CartHeader from 'components/CartHeader/CartHeader'
 
 type Props = {}
 const Header = (props: Props) => {
-    const home = false
     return (
         <AppBar
             position="static"
@@ -33,12 +32,8 @@ const Header = (props: Props) => {
                         <MenuIcon />
                     </IconButton>
                     <Logo />
-
-                    <Button color="inherit">Home</Button>
-                    <Button color="inherit">About</Button>
-                    <Button color="inherit">Payment</Button>
-                    <Button color="inherit">Shipping</Button>
-                    <Button color="inherit">Cart</Button>
+                    <Menu />
+                    <CartHeader />
                 </Toolbar>
             </Container>
         </AppBar>
